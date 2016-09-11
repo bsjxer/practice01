1039b5f3bf9efd8087f0932a5fe031a9592fdcc9
배승진 <@172.30.90.147>
2016-09-12 02:16:00 +0900

    실행이 안됨

--- src/practice01/GuessNumber.java
+++ src/practice01/GuessNumber.java
@@ -1,11 +1,50 @@
 package practice01;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessNumber {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner( System.in );
+		Random r = new Random();
+		
+		String answer = scanner.next();
+		
+		int k = r.nextInt(100) + 1;
+		
+		while(true) {
+			int a=1;
+			int b=100;
+			int i = 1;
+			
+			System.out.println("수를 결정하였습니다. 맞추어 보세요.");
+			
+			while(true) {
+				System.out.println( a + "-" + b );
+				System.out.print(i +">>");
+				int g = scanner.nextInt();
+				
+				if( g == k ) {
+					System.out.println("맞았습니다.");
+					break;
+				} else if ( g < k ) {
+					a = g;
+					System.out.println("더 높게");
+				} else if ( k < g ) {
+					b = g;
+					System.out.println("더 낮게");
+				}
+			}
+			
+			
+			System.out.print("다시하겠습니까(y/n)>>");
+			if ( answer.equals("y")) {
+			} else {
+				break;
+			}
+		}
+		
 		scanner.close();
 	}
 }
